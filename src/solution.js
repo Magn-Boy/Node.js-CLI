fa3a3fcf373191681fc7604af07edce84b8b31f9
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-05-10 16:22:43 +0300

    fix: Changed the names of the files

---
+++ src/solution.js
@@ -0,0 +1,14 @@
+// Задача на преобразование строк
+function insideOut(str) {
+    
+}
+
+// Задача на преобразование массивов
+function arrayDiff(arr1, arr2) {
+    
+}
+
+module.exports = {
+    insideOut,
+    arrayDiff
+};
