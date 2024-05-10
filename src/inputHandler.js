628e4704b9cd42524b5a94a68d3e4f45d82a15f4
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-05-10 16:29:49 +0300

    feat: Add code in flile inputHandler.js

---
+++ src/inputHandler.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+
+function readFromFile(filePath) {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        return data.trim(); 
+    } catch (err) {
+        console.error(`Error reading file: ${err}`);
+        process.exit(1); 
+    }
+}
+
+function readFromStdin() {
+    return new Promise((resolve, reject) => {
+        let data = '';
+        process.stdin.setEncoding('utf8');
+
+        process.stdin.on('readable', () => {
+            const chunk = process.stdin.read();
+            if (chunk !== null) {
+                data += chunk;
+            }
+        });
+
+        process.stdin.on('end', () => {
+            resolve(data.trim());
+        });
+    });
+}
+
+module.exports = {
+    readFromFile,
+    readFromStdin
+};
